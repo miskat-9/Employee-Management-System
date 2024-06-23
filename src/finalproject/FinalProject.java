315f6a87c8a023046dd49f3c928cad8338d42253
Miskat Raihan <59637615+miskat-9@users.noreply.github.com>
2024-06-24 03:56:52 +0600

    Add files via upload

---
+++ src/finalproject/FinalProject.java
@@ -0,0 +1,21 @@
+
+package finalproject;
+
+/**
+ *
+ * @author miska
+ */
+public class FinalProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new finalproj().setVisible(true);
+            }
+        });
+    }
+    
+}
